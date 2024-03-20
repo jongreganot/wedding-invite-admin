f1aa10cf6f6e2d28eb8ac0a84c5e0121198ff4f8
Joshua Reganot <joshuaeli.reganot@merkle.com>
2024-03-20 20:40:53 +0800

    Real data

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -65,7 +65,7 @@ class App extends React.Component {
     }
 
     fetchGuests = async () => {
-        let data = staticData;//staticData; //await fetchData("/guests");
+        let data = await fetchData("/guests");//staticData; //await fetchData("/guests");
 
         this.setState({
             unfilteredGuests: data.guests,
